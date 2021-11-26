19dc557a603ee1ad49526e1933fabf8ceb8b60ff
matiussi <matiussi305@gmail.com>
2021-11-26 13:01:44 -0300

    feat(App.vue): Importação do Header.vue e aplicação de estilos globais

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Header />
 </template>
+
+<script>
+
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100vh;
+}
+* {
+  box-sizing: border-box;
+}
+body{
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+a,
+button, 
+ul, 
+li{
+  all: unset;
+}
+a, button{
+  cursor: pointer;
+  &:focus{
+    outline: 2px solid #725bc2;;
   }
 }
 </style>
