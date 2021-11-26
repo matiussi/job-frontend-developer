0f97729a5ab1fd987fae46d882765c4a23feea7a
matiussi <matiussi305@gmail.com>
2021-11-26 11:38:05 -0300

    feat(main.js): Adicionando os ícones do Vue Unicons ao projeto

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Unicon from 'vue-unicons'
+import { uniShoppingCart, uniBars, uniSearch, uniMultiply } from 'vue-unicons/dist/icons'
 
-createApp(App).use(store).use(router).mount('#app')
+Unicon.add([uniShoppingCart, uniBars, uniSearch, uniMultiply])
+
+createApp(App)
+   .use(store)
+   .use(router)
+   .use(Unicon, {
+      height: 28,
+      width: 28 
+   })
+   .mount('#app')
