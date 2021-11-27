aa63850469ec2d90dbf7a37ff6c567ffc3288f46
matiussi <matiussi305@gmail.com>
2021-11-27 14:03:19 -0300

    feat(views/Home.vue): Importação do componente ProductsList

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <ProductsList />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductsList from '../components/ProductsList.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components:{
+    ProductsList
   }
+  
 }
+
+
 </script>
