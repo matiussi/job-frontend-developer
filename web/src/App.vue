5487bd49a9c15703c66ddfdf824858502cea6760
matiussi <matiussi305@gmail.com>
2021-11-27 14:05:13 -0300

    refactor(App.vue): Alteração na fonte principal para Raleway

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,44 +1,52 @@
 <template>
-  <Header />
+   <Header />
 </template>
 
 <script>
-
-import Header from '@/components/Header.vue'
+import Header from "@/components/Header.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    Header
-  }
-}
+   name: "Home",
+   components: {
+      Header,
+   },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  height: 100vh;
+   font-family: 'Raleway', Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   height: 100vh;
 }
 * {
-  box-sizing: border-box;
+   box-sizing: border-box;
+   /* text-transform: uppercase; */
 }
-body{
-  margin: 0;
-  padding: 0;
+body {
+   margin: 0;
+   padding: 0;
 }
-
+main {
+   margin: 0 auto;
+   padding: 40px 0px;
+   width: 100%;
+   max-width: 1300px;
+}
 a,
-button, 
-ul, 
-li{
-  all: unset;
+button,
+ul,
+li,
+figure {
+   all: unset;
 }
-a, button{
-  cursor: pointer;
-  &:focus{
-    outline: 2px solid #725bc2;;
-  }
+a,
+button {
+   cursor: pointer;
+
+   &:focus {
+      outline: 2px solid #725bc2;
+   }
 }
 </style>
