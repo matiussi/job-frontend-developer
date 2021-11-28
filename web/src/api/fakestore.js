f256a9ba7c16caf39518ce9d166813ca433a8492
matiussi <matiussi305@gmail.com>
2021-11-27 23:55:17 -0300

    feat(api/fakestore.js): Criação das funções getCategories() e getProductsByCategory()

--- web/src/api/fakestore.js
+++ web/src/api/fakestore.js
@@ -7,3 +7,21 @@ export async function getAllProducts() {
       console.log(error)
    }
 }
+export async function getCategories() {
+   try {
+      let response = await fetch('https://fakestoreapi.com/products/categories')
+      let data = await response.json()
+      return data
+   } catch (error) {
+      console.log(error)
+   }
+}
+export async function getProductsByCategory(category) {
+   try {
+      let response = await fetch(`https://fakestoreapi.com/products/category/${category}`)
+      let data = await response.json()
+      return data
+   } catch (error) {
+      console.log(error)
+   }
+}
