ae0ab6cdef089a24ec2cc539de65379cc1ceabeb
matiussi <matiussi305@gmail.com>
2021-11-28 00:06:00 -0300

    feat(components/ProductsList.vue): Busca à API de acordo com a rota e ordenação dos produtos
    A componente ProductsList irá realizar chamadas à API de acordo com a rota atual.
    - Caso a rota seja "/", então será feita uma busca para obter todos os produtos;
    - Caso a rota seja alguma categoria, então será feita uma chamada para obter os produtos daquela categoria.
    
    Ordenação de produtos por nome, preço e avaliação em ordem crescente e decrescente.

--- web/src/components/ProductsList.vue
+++ web/src/components/ProductsList.vue
@@ -1,42 +1,110 @@
 <template>
-   <div v-if="loading">
-      Loading....
+   <div class="select-container">
+      <select @change="orderProducts(orderBy)" v-model="orderBy">
+         <option disabled selected value="">Order by</option>
+         <option value="title,asc">Name, A-Z</option>
+         <option value="title,desc">Name, Z-A</option>
+         <option value="price,asc">Price, lowest</option>
+         <option value="price,desc">Price, highest</option>
+         <option value="rating,asc">Rating, lowest</option>
+         <option value="rating,desc">Rating, highest</option>
+      </select>
    </div>
+   <div v-if="loading">Loading....</div>
    <div class="list" v-if="products">
-      <ul class="products-wrapper" >
-         <Product v-bind="product" v-for="product in products" :key="product.key"/>
+      <ul class="products-wrapper">
+         <Product
+            v-bind="product"
+            v-for="product in products"
+            :key="product.key"
+         />
       </ul>
    </div>
 </template>
 
 <script>
-
-import { getAllProducts } from "../api/fakestore";
+import { getAllProducts, getProductsByCategory } from "../api/fakestore";
 import Product from "./Product.vue";
 
 export default {
    name: "ProductsList",
-   components:{
-      Product
+   components: {
+      Product,
    },
-   data(){
-      return{
+   props: {
+      page: String,
+   },
+   data() {
+      return {
          products: [],
-         loading: false
-      }
+         loading: false,
+         orderBy: "",
+      };
    },
-   async mounted() {
-      this.loading = true;
-      this.products = await getAllProducts();
-      this.loading = false;
+   created() {
+      this.fetchData();
+   },
+   watch: {
+      $route: "fetchData",
+   },
+   methods: {
+      async fetchData() {
+         this.loading = true;
+         if (this.page == "Home") {
+            this.products = await getAllProducts();
+         } else {
+            this.products = await getProductsByCategory(this.$route.params.id);
+         }
+         this.loading = false;
+      },
+      orderProducts(value) {
+         const values = value.split(",");
+         const property = values[0];
+         const type = values[1];
+
+         this.products.sort((a, b) => {
+            if (property == "title" ) {
+               return a[property].localeCompare(b[property]);
+            }
+            if(property == "price"){
+               return a[property] - b[property];
+            }
+            if(property == "rating"){
+               return a[property].rate - b[property].rate;
+            }
+         });
+         if(type == 'desc'){
+            this.products.sort().reverse();
+         }
+      },
    },
 };
 </script>
 
 <style scoped lang="scss">
-.products-wrapper{
+.products-wrapper {
    display: flex;
    flex-wrap: wrap;
    justify-content: center;
+}
+.select-container {
+   width: 100%;
+   height: 50px;
+
+   display: flex;
+   justify-content: flex-end;
+   align-items: center;
+   padding: 15px;
+
+   select{
+      width: 160px;
+      height: 40px;
+      padding-left: 10px;
+      background-color: #fff;
+      border: 1px solid #c6c6c6;
+      font-size: 16px;
+
+   }
+
 }
 </style>
