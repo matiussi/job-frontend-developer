efdcbd2b337f3bca37b318fc990b844bbacfe0b6
matiussi <matiussi305@gmail.com>
2021-11-28 15:14:06 -0300

    feat(api/fakestore.js): Retornando erro caso a requisição tenha falhado requisição seja requisição

--- web/src/api/fakestore.js
+++ web/src/api/fakestore.js
@@ -1,27 +1,29 @@
 export async function getAllProducts() {
    try {
-      let response = await fetch('https://fakestoreapi.com/products')
-      let data = await response.json()
-      return data
+      let response = await fetch('https://fakestoreapi.com/products');
+      if (!response.ok) throw response.statusText;
+
+      return response.json();
+
    } catch (error) {
-      console.log(error)
+      console.log(error);
    }
 }
 export async function getCategories() {
    try {
-      let response = await fetch('https://fakestoreapi.com/products/categories')
-      let data = await response.json()
-      return data
+      let response = await fetch('https://fakestoreapi.com/products/categories');
+      if (!response.ok) throw response.statusText;
+      return response.json();
    } catch (error) {
-      console.log(error)
+      console.log(error);
    }
 }
 export async function getProductsByCategory(category) {
    try {
-      let response = await fetch(`https://fakestoreapi.com/products/category/${category}`)
-      let data = await response.json()
-      return data
+      let response = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+      if (!response.ok) throw response.statusText;
+      return response.json();
    } catch (error) {
-      console.log(error)
+      console.log(error);
    }
 }
