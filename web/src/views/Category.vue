6394535f3c8b6562b09ab69f5d3783f3bb1ccc2f
matiussi <matiussi305@gmail.com>
2021-11-27 23:54:01 -0300

    feat(views/Category.vue): Criação da página de categorias

---
+++ web/src/views/Category.vue
@@ -0,0 +1,25 @@
+<template>
+   <main> 
+      <div class="title">
+         <h1 class="category">{{ $route.params.id }}</h1>
+      </div>
+      <ProductsList page="Category"/>
+   </main>
+</template>
+
+<script>
+import ProductsList from '../components/ProductsList.vue';
+
+export default {
+  name: 'Category',
+  components:{
+    ProductsList
+  }
+}
+</script>
+<style scoped lang="scss">
+   .category{
+      padding-left: 15px;
+      text-transform: uppercase;
+   }
+</style>
