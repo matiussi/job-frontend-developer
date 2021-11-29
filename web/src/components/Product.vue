657b24040fb089b09368ad3b1eb3adc2aba4a6e3
matiussi <matiussi305@gmail.com>
2021-11-28 21:39:45 -0300

    refactor(components/Product.vue): Redirecionamento para a rota de produto

--- web/src/components/Product.vue
+++ web/src/components/Product.vue
@@ -1,6 +1,6 @@
 <template>
    <li class="product">
-      <router-link to="/about">
+      <router-link :to="`/product/${id}`">
          <div class="image">
             <img :src="image" :alt="title" />
          </div>
