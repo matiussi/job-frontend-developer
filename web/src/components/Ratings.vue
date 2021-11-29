bf2485a398ebcffa73b97b0537fdddeb6575e320
matiussi <matiussi305@gmail.com>
2021-11-29 14:55:45 -0300

    feat(components/Ratings.vue): Componente que exibirá as avaliações dos produtos em forma de ícones

---
+++ web/src/components/Ratings.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="rating-wrapper">
+      <unicon
+         v-for="index in 5"
+         :key="index"
+         name="star"
+         :fill="Math.trunc(rate) >= index ? '#1F1D36' : '#c6c6c6'"
+         icon-style="solid"
+         :width="size"
+         :height="size"
+      >
+      </unicon>
+      <span class="rating">{{ rate }}</span>
+   </div>
+</template>
+
+<script>
+export default {
+   name: "Ratings",
+   props:{
+      rate: Number,
+      size: Number
+   }
+}
+</script>
