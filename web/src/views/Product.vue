53bf3cd2aa8b917e1a71273135db6f6b4f8dce13
matiussi <matiussi305@gmail.com>
2021-12-01 20:36:52 -0300

    feat(views/Product.vue): Animações adicionadas e exibição de feedback ao usuário

--- web/src/views/Product.vue
+++ web/src/views/Product.vue
@@ -7,36 +7,46 @@
          <div class="info-container">
             <p class="category">{{ product.category }}</p>
             <h1 class="title">{{ product.title }}</h1>
-            <Ratings 
-               :rate="product.rating.rate"
-               :size="28"
-            />
-            <p class="price">{{currency}}</p>
-         <div class="wrapper">
-         <div class="cart-options">
-            <button 
-               class="operation" 
-               @click="decreaseQuantity()"
-               :disabled="quantity <= 0 ? true : false"
-            >
-               -
-            </button>
-            <input 
-               class="quantity" 
-               type="number" 
-               :value="quantity"
-               :v-model="quantity"
-               min="0"
-               step="1"
-            />
-            <button class="operation" @click="increaseQuantity()">
-               +
-            </button>
-         </div>
-         <button class="add-to-cart" @click="addToCart(product, quantity)">
-            ADD TO CART
-         </button>
-         </div>
+            <Ratings :rate="product.rating.rate" :size="28" />
+            <p class="price">{{ currency }}</p>
+            <div class="wrapper">
+               <div class="cart-options">
+                  <button
+                     class="operation"
+                     @click="decreaseQuantity()"
+                     :disabled="quantity <= 0 ? true : false"
+                  >
+                     -
+                  </button>
+                  <input
+                     class="quantity"
+                     type="number"
+                     :value="quantity"
+                     :v-model="quantity"
+                     min="0"
+                     step="1"
+                  />
+                  <button class="operation" @click="increaseQuantity()">
+                     +
+                  </button>
+               </div>
+               <button
+                  class="add-to-cart"
+                  @click="
+                     addToCart(product, quantity);
+                     addToCartLoading();
+                  "
+                  :disabled="buttonLoading ? true : false"
+               >
+                  <unicon
+                     v-if="buttonLoading"
+                     name="spinner"
+                     fill="#ffffff"
+                  ></unicon>
+                  <p v-else>ADD TO CART</p>
+               </button>
+               <p class="success-message" v-if="success">The product has been added to cart.</p>
+            </div>
          </div>
          <div class="description-container">
             <div class="description">
@@ -45,36 +55,40 @@
             <p>{{ product.description }}</p>
          </div>
       </div>
-      <div v-if="loading">Loading....</div>
+      <Loading v-if="loading" />
       <div v-if="!loading && !product">Something went wrong...</div>
    </main>
 </template>
 
 <script>
 import { getProduct } from "../api/fakestore";
-import Ratings from '../components/Ratings.vue';
-import currency from '../utils/currency';
+import Ratings from "../components/Ratings.vue";
+import currency from "../utils/currency";
+import Loading from "../components/Loading.vue";
 
 export default {
    name: "Product",
-   components:{
-      Ratings
+   components: {
+      Ratings,
+      Loading,
    },
    data() {
       return {
          product: null,
          loading: false,
-         quantity: 1
+         quantity: 1,
+         buttonLoading: false,
+         success: false,
       };
    },
 
    mounted() {
       this.fetchData();
    },
-   computed:{
-      currency(){
+   computed: {
+      currency() {
          return currency(this.product.price);
-      }
+      },
    },
    methods: {
       async fetchData() {
@@ -82,21 +96,29 @@ export default {
          this.product = await getProduct(this.$route.params.id);
          this.loading = false;
       },
-      increaseQuantity(){
+      increaseQuantity() {
          this.quantity++;
       },
-      decreaseQuantity(){
+      decreaseQuantity() {
          this.quantity--;
       },
-      addToCart(product, quantity){
+      addToCart(product, quantity) {
          const payload = {
-            'product': product,
-            'quantity': quantity
-         }
-         this.$store.dispatch('addToCart', payload);
-      }, 
+            product: product,
+            quantity: quantity,
+         };
+         this.$store.dispatch("addToCart", payload);
+      },
+      addToCartLoading() {
+         this.buttonLoading = true;
+         this.success = false;
+         setTimeout(() => {
+            this.buttonLoading = false;
+            this.success = true;
+         }, 1000);
+         
+      },
    },
-
 };
 </script>
 <style scoped lang="scss">
@@ -111,6 +133,8 @@ export default {
       flex-direction: column;
       align-items: center;
       justify-content: center;
+      -webkit-animation: fade-in 1.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+      animation: fade-in 1.2s cubic-bezier(0.39, 0.575, 0.565, 1) both;
 
       img {
          display: inline-block;
@@ -134,24 +158,28 @@ export default {
             margin-top: -5px;
          }
       }
-      .price{
+      .price {
          font-size: 24px;
       }
+      .success-message{
+         -webkit-animation: fade-in 0.5s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+         animation: fade-in 0.5s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+         font-weight: 500;
+      }
    }
-   .cart-options{
+   .cart-options {
       display: flex;
       align-items: center;
       justify-content: flex-start;
       height: 50px;
 
-      .operation{
+      .operation {
          height: 48px;
          width: 48px;
          border: 1px solid #c6c6c6;
          font-size: 36px;
-         
       }
-      .quantity{
+      .quantity {
          height: 50px;
          width: 50px;
          text-align: center;
@@ -159,41 +187,48 @@ export default {
          border: 1px solid #c6c6c6;
       }
    }
-   .add-to-cart{
-      background-color: #1F1D36;
-      color: #fff;
+   .add-to-cart {
+      background-color: #1f1d36;
       font-weight: 500;
       font-size: 20px;
       height: 50px;
       width: 100%;
       margin: 20px 0px;
+
+      .unicon {
+         -webkit-animation: rotate-center 1s ease-in-out infinite;
+         animation: rotate-center 1s ease-in-out infinite;
+      }
+      p {
+         color: #fff;
+      }
    }
-   .description-container{
+   .description-container {
       margin-top: 60px;
       font-size: 18px;
       width: 100%;
 
-      .description{
+      .description {
          border-bottom: 1px solid #c6c6c6;
          font-weight: 500;
-         
       }
    }
 }
 
 @media screen and (min-width: 992px) {
-   .product-container{
+   .product-container {
       display: flex;
       flex-wrap: wrap;
-      
-      .image{
+
+      .image {
          width: 50%;
+         height: 100%;
       }
-      .info-container{
+      .info-container {
          width: 50%;
          flex-grow: 1;
 
-         .wrapper{
+         .wrapper {
             margin-top: 50px;
          }
       }
