29134d9da54f4ef8ace36d9637615d59f4215194
matiussi <matiussi305@gmail.com>
2021-12-01 20:31:04 -0300

    feat(components/Loading.vue): Componente responsável por exibir uma mensagem de carregamento

---
+++ web/src/components/Loading.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="loading-container">
+      <unicon name="spinner"></unicon>
+      <p class="loading">Loading...</p>
+   </div>
+</template>
+<script>
+export default {
+   name: "Loading",
+}
+</script>
+
+<style scoped lang="scss">
+.loading-container{
+   width: 100%;
+   height: 50vh;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   
+   .loading{
+      font-size: 20px;
+   }
+   .unicon {
+      -webkit-animation: rotate-center 1s ease-in-out infinite;
+      animation: rotate-center 1s ease-in-out infinite;
+   }
+}
+</style>
