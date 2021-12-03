e5c0a54163ee9f62d3c8a83ef00d087deff368b0
matiussi <matiussi305@gmail.com>
2021-12-02 22:32:00 -0300

    feat(views/Home.vue): Importando o componente SlideShow

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
-  <main>
-    <ProductsList page="Home" />
-  </main>
+   <main>
+      <SlideShow />
+      <ProductsList page="Home" />
+   </main>
 </template>
 
 <script>
-import ProductsList from '../components/ProductsList.vue';
+import ProductsList from "../components/ProductsList.vue";
+import SlideShow from "../components/SlideShow.vue";
 
 export default {
-  name: 'Home',
-  components:{
-    ProductsList
-  }
-}
+   name: "Home",
+   components: {
+      ProductsList,
+      SlideShow,
+   },
+};
 </script>
